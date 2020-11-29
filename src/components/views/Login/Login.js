a671ca856bc0779ef41a98037a0543a43b1a6588
Marcelina <m.oscislowska@gmail.com>
2020-11-29 14:55:47 +0100

    Add Login view

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
 
 const Login = () => (
-    <div className={styles.component}>
-        <h2>Login view</h2>
-    </div>
+	<form className={styles.component} noValidate autoComplete="on">
+		<Grid
+		container
+		direction="column"
+		justify="center"
+		alignItems="center"
+		>
+			<div>
+				<TextField id="outlined-basic" label="USER" variant="outlined" />
+			</div>
+			<div>
+				<TextField id="outlined-basic" label="PASSWORD" variant="outlined" />
+			</div>
+			<div>
+				<Button variant="contained">SUBMIT</Button>
+			</div>
+		</Grid>
+	</form>
 );
 
-export default Login; 
+export default Login;
