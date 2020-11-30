47a98d1cc27fbc479caad58d2568ec0a44df58dd
Marcelina <m.oscislowska@gmail.com>
2020-11-30 16:52:55 +0100

    Add Kitchen view

--- src/App.js
+++ src/App.js
@@ -37,13 +37,13 @@ class App extends React.Component {
               <Route exact path={`${process.env.PUBLIC_URL}/`} component={Homepage} />
               <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
               <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
-              <Route exact path={process.env.PUBLIC_URL + '/tables/booking/book/:id'} component={Booking} />
+              <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={Booking} />
 							<Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component={NewBooking} />
-							<Route exact path={process.env.PUBLIC_URL + '/tables/events/event/:id'} component={Events} />
+							<Route exact path={process.env.PUBLIC_URL + '/tables/events/:id'} component={Events} />
 							<Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component={NewEvents} />
               <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
 							<Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={Order} />
-							<Route exact path={process.env.PUBLIC_URL + '/order/new'} component={NewOrder} />
+							<Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={NewOrder} />
               <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
              </Switch>
            </MainLayout>
--- src/components/views/Kitchen/Kitchen.js
+++ src/components/views/Kitchen/Kitchen.js
@@ -1,10 +1,81 @@
 import React from 'react';
 import styles from './Kitchen.module.scss';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
+const demoContent = [
+  {id: '123', task: 'Zio Stefano Doughnut', amount:'3', status: 'ordered', order: 123},
+  {id: 'Table1', task: 'Zia Giulia Breakfast', amount:'1', status: 'prepared', order: 234},
+  {id: '345', task: 'Nonna Alba Pizza', amount:'2',status: 'delivered', order: 345},
+  {id: 'Bar', task: 'Nonno Alberto Salad', amount:'1',status: 'paid', order: 456},
+];
+
+const renderActions = status => {
+  switch (status) {
+    case 'ordered':
+      return (
+        <Button>ordered</Button>
+      );
+    case 'prepared':
+      return (
+        <Button>in progress</Button>
+      );
+		case 'delivered':
+			return (
+				<Button>done</Button>
+			);
+    case 'paid':
+      return null;
+    default:
+      return null;
+  }
+};
 
 const Kitchen = () => (
-    <div className={styles.component}>
-        <h2>Kitchen view</h2>
-    </div>
+  <Paper className={styles.component}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Order nr. / Table nr.</TableCell>
+          <TableCell>Status</TableCell>
+          <TableCell>Task</TableCell>
+          <TableCell>Amount</TableCell>
+          <TableCell>Action</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {demoContent.map(row => (
+          <TableRow key={row.id}>
+            <TableCell>
+              {row.id && (
+                <Button href={`${process.env.PUBLIC_URL}/waiter/order/${row.id}`}>
+                  {row.id}
+                </Button>
+              )}
+            </TableCell>
+            <TableCell>
+              {row.status}
+            </TableCell>
+            <TableCell>
+              {row.task}
+            </TableCell>
+            <TableCell component="th" scope="row">
+              {row.amount}
+            </TableCell>
+            <TableCell>
+              {renderActions(row.status)}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </Paper>
 );
 
-export default Kitchen; 
+
+export default Kitchen;
